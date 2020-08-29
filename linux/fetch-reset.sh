c3d4aef31be410052ea1188452b73ab513bd87f2
Lenovo-PC <dean0731@qq.com>
2020-08-29 20:07:13 +0800

    update git

--- linux/fetch-reset.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git fetch --all && git reset --hard origin/master && git pull
---
+++ linux/git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git fetch --all && git reset --hard origin/master && git pull
+
+#git rm -r --cached .
+#git add .
+#git commit -m 'update .gitignore'
