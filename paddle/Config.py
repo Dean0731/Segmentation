34aeee23e0cbdb6b005e2bb79faeb37b4f90263d
实验室 <1028968939@qq.com>
2020-09-26 18:40:42 +0800

    'update'

---
+++ paddle/Config.py
@@ -0,0 +1,79 @@
+import os
+import json
+
+# 设置要生成文件的路径
+data_root_path = '/home/aistudio/images/face'
+# 所有类别的信息
+class_detail = []
+# 获取所有类别保存的文件夹名称，这里是['zhangziyi', 'jiangwen', 'pengyuyan']
+class_dirs = os.listdir(data_root_path)
+# 类别标签
+class_label_dict = {'zhangziyi': 0, 'jiangwen': 1, 'pengyuyan': 2,'dzf':3}
+# 获取总类别的名称
+father_paths = data_root_path.split('/')    #['', 'home', 'aistudio', 'images', 'face']
+while True:
+    if father_paths[father_paths.__len__() - 1] == '':
+        del father_paths[father_paths.__len__() - 1]
+    else:
+        break
+father_path = father_paths[father_paths.__len__() - 1]
+# 把生产的数据列表都放在自己的总类别文件夹中
+data_list_path = '/home/aistudio/%s/' % father_path
+# 如果不存在这个文件夹,就创建
+isexist = os.path.exists(data_list_path)
+if not isexist:
+    os.makedirs(data_list_path)
+# 清空原来的数据
+with open(data_list_path + "test.list", 'w') as f:
+    pass
+with open(data_list_path + "trainer.list", 'w') as f:
+    pass
+# 总的图像数量
+all_class_images = 0
+# 读取每个类别
+for class_dir in class_dirs:
+    # 每个类别的信息
+    class_detail_list = {}
+    test_sum = 0
+    trainer_sum = 0
+    # 统计每个类别有多少张图片
+    class_sum = 0
+    # 获取类别路径
+    path = data_root_path + "/" + class_dir
+    # 获取所有图片
+    img_paths = os.listdir(path)
+
+    for img_path in img_paths:                                  # 遍历文件夹下的每个图片
+        name_path = path + '/' + img_path                       # 每张图片的路径
+        if class_sum % 10 == 0:                                 # 每10张图片取一个做测试数据
+            test_sum += 1                                       #test_sum测试数据的数目
+            with open(data_list_path + "test.list", 'a') as f:
+                f.write(name_path + "\t%d" % class_label_dict[class_dir] + "\n") #class_label 标签：0,1,2
+        else:
+            trainer_sum += 1                                    #trainer_sum测试数据的数目
+            with open(data_list_path + "trainer.list", 'a') as f:
+                f.write(name_path + "\t%d" % class_label_dict[class_dir] + "\n")#class_label 标签：0,1,2
+        class_sum += 1                                          #每类图片的数目
+        all_class_images += 1                                   #所有类图片的数目
+
+    # 说明的json文件的class_detail数据
+    class_detail_list['class_name'] = class_dir             #类别名称，如jiangwen
+    class_detail_list['class_label'] = class_label_dict[class_dir]          #类别标签，0,1,2
+    class_detail_list['class_test_images'] = test_sum       #该类数据的测试集数目
+    class_detail_list['class_trainer_images'] = trainer_sum #该类数据的训练集数目
+    class_detail.append(class_detail_list)
+
+# 获取类别数量
+all_class_sum = class_dirs.__len__()
+# 说明的json文件信息
+readjson = {}
+readjson['all_class_name'] = father_path                  #文件父目录
+readjson['all_class_sum'] = all_class_sum                #
+readjson['all_class_images'] = all_class_images
+readjson['class_detail'] = class_detail
+jsons = json.dumps(readjson, sort_keys=True, indent=4, separators=(',', ': '))
+with open(data_list_path + "readme.json",'w') as f:
+    f.write(jsons)
+print ('生成数据列表完成！')
+print ("标签及其类别：{}".format(class_label_dict))
+
---
+++ util/datasetTools.py
@@ -0,0 +1,151 @@
+# @Time     : 2020/8/30 22:43
+# @File     : generate_data_txt
+# @Email    : dean0731@qq.com
+# @Software : PyCharm
+# @Desc     :
+# @History  :
+#   2020/8/30 Dean First Release
+
+import os
+from PIL import Image
+import tifffile as tiff  # 也可使用pillow或opencv 但若图片过大时可能会出问题
+from random import randint
+def rgbTo256(src,filter):
+    """
+    将文件夹中的rgb图片，转换为256色图片，labelme的标签格式
+    只能转换png格式，
+    @params: src
+    """
+    def randomPalette(length, min, max):
+        return [randint(min, max) for x in range(length)]
+    k = 1
+    for root, dirs, files in os.walk(src, topdown=False):
+        for name in files:
+            path = os.path.join(root,name)
+            if filter in path:
+                img = Image.open(path)
+                img = img.convert('P')
+                i = randomPalette(0, 0, 0)
+                img.putpalette(i)
+                img.save(path)
+                print(k,path)
+                k = k+1
+
+def reformate(src):
+    """
+    将源文件改编为png格式
+    """
+    i = 0
+    for root, dirs, files in os.walk(src, topdown=False):
+        for name in files:
+            img = Image.open(os.path.join(root,name))
+            i=i+1
+            name,format = name.split('.')
+            newname = name+'.png'
+            path = os.path.join(root,newname)
+            img.save(path)
+            print(i,path)
+def generate(src,img,label):
+    with open(os.path.join(src,'data.txt'),'w',encoding='utf-8') as f:
+        for root, dirs, files in os.walk(src, topdown=False):
+            for name in files:
+                if not label in root and not 'data.txt' in name:
+                    root = root.replace(src,'')
+                    f.write("{};{}\n".format(os.path.join(root,name),os.path.join(root.replace(img,label),name)))
+
+#-------------------------------------------------------------------
+# 数据集切割脚本，法国遥感房屋数据集 https://project.inria.fr/aerialimagelabeling/contest/
+
+
+width = 576   # 切割图像大小
+height = 576  # 切割图像大小
+iou =  228  # 交叉切割
+file_home = r"G:\AI_dataset\马萨诸塞州-房屋数据集1\Massachusetts Buildings Dataset\Training Set\Target maps"
+file_names = os.listdir(file_home)
+target_home = r'G:\AI_dataset\马萨诸塞州-房屋数据集1\Massachusetts Buildings Dataset2\Training Set\Target maps'
+if not os.path.exists(target_home):
+    os.makedirs(target_home)
+
+for i,file in enumerate(file_names):
+    img = tiff.imread(os.path.join(file_home,file))  # 导入图片
+    print("第{}导入图片完成".format(i),img.shape) # 原始图片大小
+    pic_width = img.shape[1]
+    pic_height = img.shape[0]
+    col,col_end,row,row_end=0,0,0,0
+    k = 0
+    while True:
+        row=row_end
+        row_end = row_end+height
+        if row_end > pic_height:
+            break
+        while True:
+            col=col_end
+            col_end=col_end+width
+            if col_end > pic_width:
+                break;
+            cropped=img[col:col_end,row:row_end]
+            name = "{}_{}.tif".format(file.split(".")[0],str(k).rjust(3,'0'))
+            image_path = os.path.join(target_home,name)
+            tiff.imsave(image_path, cropped)
+            print("第{}图片的第{}个分割:{}".format(i,k,image_path))
+            col_end=col_end-iou
+            k = k+1
+        row_end = row_end-iou
+        col,col_end=0,0
+#-----------------------------------------
+
+# desc:将label文件夹中的laebl提取出来
+target_dir = r"I:\AI_dataset\DOM\米庄村-DOM\image-3000\json"  # json_label 所在的文件夹
+files = [os.path.join(target_dir,file) for file in os.listdir(target_dir)]
+for i in files:
+    if os.path.isdir(i):
+        lables = os.listdir(i)
+        for file in lables:
+            if file == "label.png":
+                image_path = os.path.join(i, "label.png")
+                imgae = Image.open(image_path)
+                parent_dir_name = os.path.basename(os.path.dirname(image_path))
+                new_name = "{}.png".format(parent_dir_name.rsplit("_",1)[0])
+                imgae.save(os.path.join(target_dir,new_name))
+                print("第{}个文件夹".format(i))
+                break
+# --------------------------------------
+# desc:批量将json文件转为 label
+# linux_dir = r"/media/dean/Document/AI_dataset/DOM/裴庄村51-dom/image-3000"
+windows_dir = r"I:\AI_dataset\DOM\米庄村-DOM\image-3000\json"
+dir = windows_dir
+files = [os.path.join(dir,file) for file in os.listdir(dir) if file.endswith(".json")]
+for file in files:
+    cmd = "labelme_json_to_dataset {}".format(file)
+    print(cmd)
+    os.system(cmd)
+
+
+# ------------------------------------------------------------
+# Desc: 修改数据的大小或格式
+def main(src,des,target_size,target_format):
+    i = 0
+    for root, dirs, files in os.walk(src, topdown=False):
+        temp = root.split('\\segmentation\\')
+        for name in files:
+            img = Image.open(os.path.join(root,name))
+            if target_size != None:
+                img = img.resize(target_size,resample=Image.NEAREST)
+            i=i+1
+            name,format = name.split('.')
+            newroot = os.path.join(des,temp[1])
+            if target_format !=None:
+                if 'tif' in format:
+                    format=target_format
+            newname = name+'.'+format
+            if not os.path.exists(newroot):
+                os.makedirs(newroot)
+            path = os.path.join(newroot,newname)
+            # img.save(path)
+            print(i,path)
+
+if __name__ == '__main__':
+    src = r'G:\AI_dataset\dom\segmentation'
+    des = r'G:\AI_dataset\dom\segmentation3'
+    target_format = 'png'
+    # main(src,des,target_size=None,target_format=target_format)
