bdf06d2e0e49abf86754c7cfed90b135f8a517b5
执念 <oncwnuGN3m1eZc9PdRyrtZA_R1yM@git.weixin.qq.com>
2020-11-22 18:29:37 +0800

    'update'

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from pytorch.network.deeplabv3plus import modeling
-model = modeling.deeplabv3_resnet50(2, pretrained_backbone=False)
+from pytorch.network.deeplabv3plus.backbone import resnet
+model = resnet.__dict__['resnet50'](
+    pretrained=False,
+    replace_stride_with_dilation=[False, True, True])
 from torchsummary import summary
 summary(model,(3,512,512))
