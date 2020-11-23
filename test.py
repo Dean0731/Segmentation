5405b0690509867934512624987b240821918b45
执念 <oncwnuGN3m1eZc9PdRyrtZA_R1yM@git.weixin.qq.com>
2020-11-23 18:09:34 +0800

    update

--- test.py
+++ test.py
@@ -1,59 +1,44 @@
-from pytorch.network.deeplabv3plus._deeplab import DeepLabHead, DeepLabHeadV3Plus, DeepLabV3,ASPP2
-import torch
-if __name__ == '__main__':
-    front = ASPP2(3, [12, 24, 36]).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-    from torchsummary import summary
-    summary(front,input_size=(3,512,512))
+from PIL import Image
+import numpy as np
+import util
+def max_pooling_forward(z, pooling, strides=(2, 2), padding=(0, 0)):
+    """
+    最大池化前向过程
+    :param z: 卷积层矩阵,形状(N,C,H,W)，N为batch_size，C为通道数
+    :param pooling: 池化大小(k1,k2)
+    :param strides: 步长
+    :param padding: 0填充
+    :return:
+    """
+    N, C, H, W = z.shape
+    # 零填充
+    padding_z = np.lib.pad(z, ((0, 0), (0, 0), (padding[0], padding[0]), (padding[1], padding[1])), 'constant', constant_values=0)
 
-from torch import nn
+    # 输出的高度和宽度
+    out_h = (H + 2 * padding[0] - pooling[0]) // strides[0] + 1
+    out_w = (W + 2 * padding[1] - pooling[1]) // strides[1] + 1
 
-class CSDN_Tem(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(CSDN_Tem, self).__init__()
-        self.depth_conv = nn.Conv2d(
-            in_channels=in_ch,
-            out_channels=in_ch,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            groups=in_ch
-        )
-        self.point_conv = nn.Conv2d(
-            in_channels=in_ch,
-            out_channels=out_ch,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            groups=1
-        )
+    pool_z = np.zeros((N, C, out_h, out_w))
 
-    def forward(self, input):
-        out = self.depth_conv(input)
-        out = self.point_conv(out)
-        return out
-
-class ASPP(nn.Module):
-    def __init__(self, in_channels, atrous_rates):
-        super(ASPP, self).__init__()
-        out_channels = 256
-        modules = []
-        rate1, rate2, rate3 = tuple(atrous_rates)
-        modules.append(ASPPConv(in_channels, out_channels, rate1))
-        modules.append(ASPPConv(in_channels, out_channels, rate2))
-        modules.append(ASPPConv(in_channels, out_channels, rate3))
-        modules.append(ASPPPooling(in_channels, out_channels))
-
-        self.convs = nn.ModuleList(modules)
-
-        self.project = nn.Sequential(
-            nn.Conv2d(5 * out_channels, out_channels, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.1),)
-
-    def forward(self, x):
-        res = []
-        for conv in self.convs:
-            res.append(conv(x))
-        res = torch.cat(res, dim=1)
-        return self.project(res)
+    for n in np.arange(N):
+        for c in np.arange(C):
+            for i in np.arange(out_h):
+                for j in np.arange(out_w):
+                    pool_z[n, c, i, j] = np.max(padding_z[n, c,
+                                                strides[0] * i:strides[0] * i + pooling[0],
+                                                strides[1] * j:strides[1] * j + pooling[1]])
+    return pool_z
+path = r'C:\Users\root\OneDrive - bit.edu.cn\桌面\毕业设计\temp\test.jpg'
+img = Image.open(path)
+img = np.array(img)
+img = np.expand_dims(img,axis=0)
+print(img.shape)
+img = img.transpose(0,3,1,2)
+print(img.shape)
+img = max_pooling_forward(img,(2,2))
+print(img.shape)
+img = img.transpose(0,2,3,1)
+print(img.shape)
+img = img[0,:,:,:]
+seg_img = Image.fromarray(np.uint8(img)).convert('RGB')
+seg_img.save(r"C:\Users\root\OneDrive - bit.edu.cn\桌面\毕业设计\temp\test-maxpool.jpg")
