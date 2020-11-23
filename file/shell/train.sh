5c710b9028dde1d1191418600bb36713b3830130
执念 <oncwnuGN3m1eZc9PdRyrtZA_R1yM@git.weixin.qq.com>
2020-11-23 15:25:02 +0800

    update

--- file/shell/train.sh
+++
@@ -1,6 +0,0 @@
-nohup python main.py --type pytorch --epochs 80  --info 1 --log 1 >train.log 2>&1 &
-nohup python main.py --type pytorch --epochs 80  --info 0 --log 1 >train.log 2>&1 &
-
-
-#python main.py --type pytorch --epochs 2 --info 0
-#python main.py --type pytorch --epochs 2 --info 0 --log 1
